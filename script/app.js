af3c54abc58185f4eb1d9d7d53bbde995d7188a2
D1183-Mustafa <93746275+D1183-Mustafa@users.noreply.github.com>
2022-02-16 05:58:44 +0300

    js dosyasında listeye eleman ekleme işlemi gerçekleştirildi

--- script/app.js
+++ script/app.js
@@ -1,4 +1,20 @@
 const input = document.getElementById("input");
 const btn = document.getElementById("btn");
 const liste_ul = document.querySelector(".liste_ul");
-const
+
+btn.addEventListener("click", () => {
+  let img = document.createElement("img");
+  img.setAttribute("src", "./img/red.png");
+  img.style.width = "20px";
+
+  let p = document.createElement("p");
+  p.appendChild(document.createTextNode(input.value));
+
+  let li = document.createElement("li");
+  li.setAttribute("class", "liste_ul_li");
+
+  li.appendChild(img);
+  li.appendChild(p);
+  liste_ul.appendChild(li);
+});
+
